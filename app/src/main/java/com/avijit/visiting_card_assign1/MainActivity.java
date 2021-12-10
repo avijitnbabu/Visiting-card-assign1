ff7f06a7f7adb91f7acb937f040d1659715e1cc9
codejasbrar <jasbrar585@gmail.com>
2021-12-10 13:59:35 +0600

    Initial commit

---
+++ app/src/main/java/com/avijit/visiting_card_assign1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.avijit.visiting_card_assign1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+////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
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
